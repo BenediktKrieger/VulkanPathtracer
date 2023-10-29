6639ab171288169227a9e951351717197e208161
BenediktKrieger <benediktkrieger@uni-koblenz.de>
2023-10-29 20:22:20 +0100

    first running commit

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,10 +1,8 @@
-﻿#include <glm/glm.hpp>
-#include <SDL.h>
+﻿#include <SDL.h>
 #include <SDL_vulkan.h>
-
-#include "vk_engine.h"
-#include "vk_types.h"
-#include "vk_initializers.h"
+#include <vk_engine.h>
+#include <vk_types.h>
+#include <vk_initializers.h>
 
 void VulkanEngine::init()
 {
